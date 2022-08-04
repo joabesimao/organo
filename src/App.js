076d7cf88ccd44693e229ae538291f80597bf658
joabesimao <simaojoabecosta@gmail.com>
2022-08-04 15:14:02 -0300

    atualiza o app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
-import "./App.css";
 import { Banner } from "./components/Banner";
+import { Form } from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Form />
     </div>
   );
 }
