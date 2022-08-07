73d2b2be08fde9ac3e207ffccf381750ea95a2f9
joabesimao <simaojoabecosta@gmail.com>
2022-08-07 08:20:57 -0300

    pega valor no input

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,10 +1,23 @@
 import "./TextField.css";
 
 export const TextField = (props) => {
+  let valueInput = "Joabe";
+
+  const aoDigitado = (event) => {
+    valueInput = event.target.value;
+
+    console.log(valueInput);
+  };
+
   return (
     <div className="textField">
       <label>{props.label}</label>
-      <input placeholder={props.placeholder} />
+      <input
+        value={valueInput}
+        onChange={aoDigitado}
+        required={props.mandatory}
+        placeholder={props.placeholder}
+      />
     </div>
   );
 };
