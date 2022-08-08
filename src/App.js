f5930726d0467ce1d17bb8038f845ed82ac147fe
joabesimao <simaojoabecosta@gmail.com>
2022-08-08 19:40:25 -0300

    cria toTheCollaborator e componente Team

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import { Banner } from "./components/Banner";
 import { Form } from "./components/Form";
+import { Team } from "./components/Team";
 
 function App() {
+  const [collaborators, setCollaborators] = useState([]);
+
+  const newCollaboratorAdd = (collaborator) => {
+    console.log(collaborator);
+    setCollaborators([...collaborators, collaborator]);
+  };
+
   return (
     <div className="App">
       <Banner />
-      <Form />
+      <Form
+        toTheCollaborator={(collaborator) => newCollaboratorAdd(collaborator)}
+      />
+      <Team name="Presidente" />
+      <Team name="Diretor Administrativo" />
+      <Team name="Diretor Comercial" />
     </div>
   );
 }
