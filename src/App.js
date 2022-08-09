89b596c96f09d71a11aea9168ab724a416740ea5
joabesimao <simaojoabecosta@gmail.com>
2022-08-09 18:53:14 -0300

    cria squad

--- src/App.js
+++ src/App.js
@@ -4,6 +4,49 @@ import { Form } from "./components/Form";
 import { Team } from "./components/Team";
 
 function App() {
+  const squad = [
+    {
+      name: "Presidente",
+      colorPrimary: "#57c278",
+      colorSecundary: "yellow",
+    },
+    {
+      name: "Diretor Administrativo",
+      colorPrimary: "#82cffa",
+      colorSecundary: "purple",
+    },
+    {
+      name: "Diretor Comercial",
+      colorPrimary: "#f0f8e2",
+      colorSecundary: "#a6d157",
+    },
+    {
+      name: "Gerente de Vendas",
+      colorPrimary: "#fde7e8",
+      colorSecundary: "#ed6869",
+    },
+    {
+      name: "Gerente de Operações",
+      colorPrimary: "#fae9f5",
+      colorSecundary: "#db6ebf",
+    },
+    {
+      name: "Gerente de Marketing",
+      colorPrimary: "#57c278",
+      colorSecundary: "white",
+    },
+    {
+      name: "Vendedores",
+      colorPrimary: "#82cffa",
+      colorSecundary: "purple",
+    },
+    {
+      name: "Serviço Tecnico",
+      colorPrimary: "#f0f8e2",
+      colorSecundary: "#a6d157",
+    },
+  ];
+
   const [collaborators, setCollaborators] = useState([]);
 
   const newCollaboratorAdd = (collaborator) => {
@@ -17,9 +60,14 @@ function App() {
       <Form
         toTheCollaborator={(collaborator) => newCollaboratorAdd(collaborator)}
       />
-      <Team name="Presidente" />
-      <Team name="Diretor Administrativo" />
-      <Team name="Diretor Comercial" />
+      {squad.map((squad) => (
+        <Team
+          key={squad.name}
+          name={squad.name}
+          colorPrimary={squad.colorPrimary}
+          colorSecundary={squad.colorSecundary}
+        />
+      ))}
     </div>
   );
 }
