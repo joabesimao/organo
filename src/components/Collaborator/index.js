95f9ef4489b8b8881cec4bd6a090d4c9c3a265e4
joabesimao <simaojoabecosta@gmail.com>
2022-08-09 19:32:06 -0300

    cria index e Colaborador.css

---
+++ src/components/Collaborator/index.js
@@ -0,0 +1,15 @@
+import "./Collaborator.css";
+
+export const Collaborator = () => {
+  return (
+    <div className="coll">
+      <div className="header">
+        <img src="https://github.com/joabesimao.png" alt="Simao" />
+      </div>
+      <div className="footer">
+        <h4>Joabe Simao</h4>
+        <h5>Dev</h5>
+      </div>
+    </div>
+  );
+};
