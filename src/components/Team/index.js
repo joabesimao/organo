252603870bcf6202f1ff4b3d6978b702e2551ce1
joabesimao <simaojoabecosta@gmail.com>
2022-08-09 19:31:25 -0300

    cria Colaborador

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,9 +1,11 @@
+import { Collaborator } from "../Collaborator";
 import "./Team.css";
 
 export const Team = (props) => {
   return (
     <section className="squad" style={{ backgroundColor: props.colorPrimary }}>
       <h3 style={{ borderColor: props.colorSecundary }}>{props.name}</h3>
+      <Collaborator />
     </section>
   );
 };
