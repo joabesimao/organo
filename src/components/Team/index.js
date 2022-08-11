3c0768c2d210f1578f546bbbcff0bd9b4c96eb73
joabesimao <simaojoabecosta@gmail.com>
2022-08-11 07:53:06 -0300

    mostra somente os times que tem colaboradores

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -3,9 +3,22 @@ import "./Team.css";
 
 export const Team = (props) => {
   return (
-    <section className="squad" style={{ backgroundColor: props.colorPrimary }}>
-      <h3 style={{ borderColor: props.colorSecundary }}>{props.name}</h3>
-      <Collaborator />
-    </section>
+    props.collaborators.length > 0 && (
+      <section
+        className="squad"
+        style={{ backgroundColor: props.colorPrimary }}
+      >
+        <h3 style={{ borderColor: props.colorSecundary }}>{props.name}</h3>
+        <div className="collaborators">
+          {props.collaborators.map((collaborators) => (
+            <Collaborator
+              name={collaborators.name}
+              office={collaborators.office}
+              image={collaborators.image}
+            />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
