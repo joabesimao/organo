7630f9b96b913503d5a8f6deec7003bdf2b08a52
joabesimao <simaojoabecosta@gmail.com>
2022-08-26 19:34:39 -0300

    modifica props.nome,cargo e imagem

--- src/components/Collaborator/index.js
+++ src/components/Collaborator/index.js
@@ -4,11 +4,11 @@ export const Collaborator = (props) => {
   return (
     <div className="coll">
       <div className="header" style={{ backgroundColor: props.color }}>
-        <img src={props.image} alt={props.name} />
+        <img src={props.imagem} alt={props.nome} />
       </div>
       <div className="footer">
-        <h4>{props.name}</h4>
-        <h5>{props.office}</h5>
+        <h4>{props.nome}</h4>
+        <h5>{props.cargo}</h5>
       </div>
     </div>
   );
